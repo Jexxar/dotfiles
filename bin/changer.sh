757e6daccb6c9fbbb1c6ce4bb8844e7c7648235a
Jexxar <Jexxar@users.noreply.github.com>
2018-04-06 05:17:07 -0300

    changer.sh

--- bin/changer.sh
+++ bin/changer.sh
@@ -6,39 +6,43 @@ function is_running_X(){
     return 0
 }
 
-if [ is_running_X ]; then
-    if [ -z $DISPLAY ]; then
-        DISPLAY=:0.0
-    fi
-else
-    echo "No X server at \$DISPLAY [$DISPLAY]" >&2
-    exit 0
-fi
+function change_wallpaper(){
+	local old_IFS=$IFS
+	IFS="
+	"
+	local WallDir="$HOME/Imagens/Wallpaper"
+	local WallList=( `find "$WallDir" -type f -iregex ".*/.*[.]\(jpe?g\|png\|gif\|bmp\)"` )
+	IFS=$old_IFS
+	local MaxFiles=${#WallList[@]}
+	#let "number = $RANDOM"
+	#let CurrIndex="`cat $WallDir/.last` + $number"
+	local LastFile="$WallDir/.last"
+	if [ -f $LastFile ]; then 
+	    let CurrIndex="`cat $LastFile` + 1"
+	    if [ $CurrIndex -ge $MaxFiles ]; then
+	        let CurrIndex=1
+	    fi
+	else
+	    let CurrIndex=1
+	fi
+	local number
+	let number=$CurrIndex
+	echo $number > $WallDir/.last
+	nitrogen --set-zoom-fill --save "${WallList[$number]}"
+}
 
-old_IFS=$IFS
-IFS="
-"
-WALLPAPERS="$HOME/Imagens/Wallpaper"
-#ALIST=( `ls -w1 $WALLPAPERS` )
-ALIST=( `find "$WALLPAPERS" -type f -iregex ".*/.*[.]\(jpe?g\|png\|gif\|bmp\)"` )
-IFS=$old_IFS
-RANGE=${#ALIST[@]}
-#let "number = $RANDOM"
-#let LASTNUM="`cat $WALLPAPERS/.last` + $number"
-last_f="$WALLPAPERS/.last"
-if [ -f $last_f ]; then 
-    let LASTNUM="`cat $last_f` + 1"
-    if [ $LASTNUM -ge $RANGE ]; then
-        let LASTNUM=1
-    fi
-else
-    let LASTNUM=1
-fi
-#let "number = $LASTNUM % $RANGE"
-let number=$LASTNUM
-echo $number > $WALLPAPERS/.last
-#echo "range == $RANGE"
-#echo "number == $number"
-#echo "wallpaper == ${ALIST[$number]}"
-#nitrogen --set-scaled --save "${ALIST[$number]}"
-nitrogen --set-zoom-fill --save "${ALIST[$number]}"
+function main(){
+	while [ is_running_X ]; do 
+	    if [ -z $DISPLAY ]; then
+    	    DISPLAY=:0.0
+    	fi
+		change_wallpaper;
+		sleep 5m;
+	done;	
+	if [ ! is_running_X ]; then
+    	echo "No X server at \$DISPLAY [$DISPLAY]" >&2
+    	exit 0
+	fi
+}
+
+main
