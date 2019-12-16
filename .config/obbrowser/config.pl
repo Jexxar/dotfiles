0c407e050101fc74ae72e0aceea68cbe258ff2fa
Jexxar <Jexxar@users.noreply.github.com>
2019-12-15 23:21:58 -0300

    upd201915a

---
+++ .config/obbrowser/config.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+
+# obbrowser - configuration file
+# This file is updated automatically.
+# Any additional comment and/or indentation will be lost.
+
+=for comment
+
+|| ICON SETTINGS
+    | with_icons       : A true value will make the script to use icons for files and directories.
+                         This option may be slow, depending on the configuration of your system.
+
+    | mime_ext_only    : A true value will make the script to get the mimetype by extension only.
+                         This will improve the performance, as no content will be read from files.
+
+    | icon_size        : Preferred size for icons. (default: 48)
+    | generic_fallback : Try to shorten icon name at '-' characters before looking at inherited themes. (default: 1)
+
+|| MENU
+    | terminal         : Command to a terminal emulator for opening directories.
+    | file_manager     : Command to a file manager for opening files and directories.
+    | browse_label     : Label for "Browse here..." action.
+    | terminal_label   : Laber for "Terminal here..." action.
+    | with_terminal    : Include the "Terminal here..." menu entry, to open directories in a terminal.
+    | start_path       : Absolute path from which to start to browse the filesystem.
+    | dirs_first       : Order directories before files.
+    | ignore_dotfiles  : Ignore files starting with a dot.
+
+=cut
+
+our $CONFIG = {
+  browse_label      => "Abrir...",
+  dirs_first        => 1,
+  file_manager      => "xdg-open",
+  file_manager_icon => "folder-open",
+  generic_fallback  => 1,
+  icon_size         => 48,
+  ignore_dotfiles   => 1,
+  mime_ext_only     => 0,
+  start_path        => "$ENV{HOME}",
+  terminal          => "tilix -d",
+  terminal_icon     => "utilities-terminal",
+  terminal_label    => "Terminal aqui...",
+  VERSION           => 0.09,
+  with_icons        => 1,
+  with_terminal     => 0,
+}
