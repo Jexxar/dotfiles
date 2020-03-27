e34f9a202c4292795c506dcd2ec4e0518ea640ce
Jexxar <Jexxar@users.noreply.github.com>
2020-03-27 06:28:39 -0300

    cleanup

--- .config/dmenu/dmenu-bind.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#exe=`dmenu_path | dmenu_run  -l 5  -i  -fn sans-8 -nb '#EEEEEE' -nf '#474747' -sb '#868686' -sf '#FEFEFE'` #&& eval "exec $exe"
-#echo "$exe"
-####!/bin/bash
-exe=`dmenu_path | dmenu -b -nb '#151617' -nf '#d8d8d8' -sb '#d8d8d8' -sf '#151617'` && eval "exec $exe"
